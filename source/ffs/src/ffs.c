13107fb408fe2c54be784cc881eaf5a89b16afe3
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-05-17 11:18:39 +0200

    ffs: Add ffs sub-command sub-commands skeletons
    Each skeleton provides only a placeholder for the "create" operation.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- source/ffs/src/ffs.c
+++ source/ffs/src/ffs.c
@@ -25,9 +25,109 @@ int gt_ffs_help(void *data)
 	return -1;
 }
 
+static int gt_ffs_interface_help(void *data)
+{
+	printf("FFS interface help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+static int gt_ffs_endpoint_help(void *data)
+{
+	printf("FFS endpoint help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+static int gt_ffs_language_help(void *data)
+{
+	printf("FFS language help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+static int gt_ffs_string_help(void *data)
+{
+	printf("FFS string help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+static int gt_ffs_interface_create_help(void *data)
+{
+	printf("FFS interface create help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+const Command *gt_ffs_interface_get_children(const Command *cmd)
+{
+	static Command commands[] = {
+		{"create", NEXT, NULL, NULL, gt_ffs_interface_create_help},
+		CMD_LIST_END
+	};
+
+	return commands;
+}
+
+static int gt_ffs_endpoint_create_help(void *data)
+{
+	printf("FFS endpoint create help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+const Command *gt_ffs_endpoint_get_children(const Command *cmd)
+{
+	static Command commands[] = {
+		{"create", NEXT, NULL, NULL, gt_ffs_endpoint_create_help},
+		CMD_LIST_END
+	};
+
+	return commands;
+}
+
+static int gt_ffs_language_create_help(void *data)
+{
+	printf("FFS language create help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+const Command *gt_ffs_language_get_children(const Command *cmd)
+{
+	static Command commands[] = {
+		{"create", NEXT, NULL, NULL, gt_ffs_language_create_help},
+		CMD_LIST_END
+	};
+
+	return commands;
+}
+
+static int gt_ffs_string_create_help(void *data)
+{
+	printf("FFS string create help func. Not implemented yet.\n");
+
+	return -1;
+}
+
+const Command *gt_ffs_string_get_children(const Command *cmd)
+{
+	static Command commands[] = {
+		{"create", NEXT, NULL, NULL, gt_ffs_string_create_help},
+		CMD_LIST_END
+	};
+
+	return commands;
+}
+
 const Command *gt_ffs_get_children(const Command *cmd)
 {
 	static Command commands[] = {
+		{"interface", NEXT, command_parse, gt_ffs_interface_get_children, gt_ffs_interface_help},
+		{"endpoint", NEXT, command_parse, gt_ffs_endpoint_get_children, gt_ffs_endpoint_help},
+		{"language", NEXT, command_parse, gt_ffs_language_get_children, gt_ffs_language_help},
+		{"string", NEXT, command_parse, gt_ffs_string_get_children, gt_ffs_string_help},
 		CMD_LIST_END
 	};
 
